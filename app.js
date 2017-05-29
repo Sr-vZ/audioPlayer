4417f05e3839d28b5889f3ad20da0bd9b4e17e80
SrvZ <srvz39@gmail.com>
2017-05-29 22:04:40 +0530

    adding backend logic

--- app.js
+++ app.js
@@ -2,5 +2,21 @@
 var sound = new Howl({
   src: ['https://dl.pagal.link/upload_file/5570/6757/Latest Bollywood Hindi Mp3 Songs - 2017/Sachin A Billion Dreams %282017%29 Hindi Mp3 Songs/01 Hind Mere Jind - Sachin %28AR Rahman%29 320Kbps.mp3']
 });
+var isPlaying=false;
 
-sound.play();
+function playSound(){
+    var playEle = document.getElementById('play');
+    if(isPlaying==false){
+        sound.play();
+        isPlaying=true;
+    }else{
+        sound.pause();
+        isPlaying=false;
+    }
+};
+
+$('document').ready(function(){
+    $('play').bind('onclick',function(){
+        playSound();
+    });
+});
