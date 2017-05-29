5c9cbeefc413bd669d16a3c2342dfe33e19f9448
SrvZ <srvz39@gmail.com>
2017-05-29 21:28:41 +0530

    initial boilerplate in place

---
+++ app.js
@@ -0,0 +1,6 @@
+
+var sound = new Howl({
+  src: ['https://dl.pagal.link/upload_file/5570/6757/Latest Bollywood Hindi Mp3 Songs - 2017/Sachin A Billion Dreams %282017%29 Hindi Mp3 Songs/01 Hind Mere Jind - Sachin %28AR Rahman%29 320Kbps.mp3']
+});
+
+sound.play();
