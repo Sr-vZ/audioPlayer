e07881d68cac0ac26cd40900e7198aaac93636e2
SrvZ <srvz39@gmail.com>
2017-06-07 09:46:07 +0530

    added mediaTag to grab album art :) !!!

---
+++ mediaTag.js
@@ -0,0 +1,28 @@
+// Simple API - will fetch all tags 
+var jsmediatags = require("jsmediatags");
+ 
+jsmediatags.read("./media/Hind Mere Jind.mp3", {
+  onSuccess: function(tag) {
+    console.log(tag);
+    albumArt(tag.tags);
+  },
+  onError: function(error) {
+    console.log(':(', error.type, error.info);
+  }
+});
+
+function albumArt(tags){
+    var image = tags.picture;
+      if (image) {
+        var base64String = "";
+        for (var i = 0; i < image.data.length; i++) {
+            base64String += String.fromCharCode(image.data[i]);
+        }
+        var base64 = "data:" + image.format + ";base64," +
+                window.btoa(base64String);
+        document.getElementById('album-art').setAttribute('src',base64);
+      } else {
+        document.getElementById('album-art').style.display = "none";
+        console.log("failed!");
+      }
+    }
