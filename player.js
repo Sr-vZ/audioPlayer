c2b2a7e0ece550c1e5feaa9449f3122a9502fe8f
SrvZ <srvz39@gmail.com>
2017-06-08 10:28:33 +0530

    added responsiveness

---
+++ player.js
@@ -0,0 +1,50 @@
+var sound = new Howl({
+    src: ['./media/Hind Mere Jind.mp3'],
+    html5: false,
+});
+
+var isPlaying = false;
+
+var refreshSeek;
+
+function playSound() {
+    var playEle = document.getElementById('play');
+    if (sound.playing() === false) {
+        sound.play();
+        //isPlaying = true;
+        $('#play-icon').toggleClass('glyphicon glyphicon-pause').toggleClass('glyphicon glyphicon-play');
+        refreshSeek = setInterval(seekBar, 1000);
+        //$('#duration').innerHtml=sound.duration();
+    } else {
+        sound.pause();
+        //isPlaying = false;
+        //$('#play-icon').toggleClass('glyphicon glyphicon-pause');
+        $('#play-icon').toggleClass('glyphicon glyphicon-pause').toggleClass('glyphicon glyphicon-play');
+        clearInterval(refreshSeek);
+    }
+    
+}
+
+
+function seekBar() {
+
+    $("#progress_bar").css('width', (sound.seek() / sound.duration()) * 100);
+    //console.log(sound.seek() / sound.duration());
+
+}
+
+function updateAnimations() {
+    seekBar();
+}
+
+function togglePlayicon(){
+        $('#play-icon').toggleClass('glyphicon glyphicon-pause').toggleClass('glyphicon glyphicon-play');
+}
+$(document).ready(function() {
+    
+    $('#play').bind('click', function() {
+        playSound();
+        //visualize();
+    });
+    visualize2();
+});
