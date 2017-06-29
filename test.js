1673fcf61337d2602b066c2602d87e3ccfe6e946
Sourav De <srvz39@gmail.com>
2017-06-29 18:52:01 +0530

    added method for NoembedSouncloud

--- test.js
+++ test.js
@@ -1,34 +1,14 @@
-    pools = [
-         "http://www.marmiton.org/recettes/recette_salade-de-betteraves-a-l-orientale_16831.aspx",
-         "http://www.marmiton.org/recettes/recette_pain-d-epices-a-la-dijonnaise_16832.aspx",
-         "http://www.marmiton.org/recettes/recette_tarte-au-chocolat-et-creme-moka_16834.aspx",
-         "http://www.marmiton.org/recettes/recette_poulet-a-la-gaston-gerard_16836.aspx",
-       "http://www.marmiton.org/recettes/recette_assiette-paula_16837.aspx"]
-    
-        var request = require("request");
-        var cheerio = require("cheerio");
-        proxy='http://proxy.intra.bt.com:8080';
-        var poolsLength = pools.length;
-        var interval = 10 * 1000; // 10 seconds;
-        for (var i = 0 ; i < pools.length ; i++) {
-           var url = pools[i];
-           setTimeout( function (i) {
-            request({'url':url,'proxy':proxy, function (error, response, body) {
-             if (!error) {
-            var $ = cheerio.load(body,{
-              ignoreWhitespace: true
-        });
-           var name = [];
-           var address = [];
-           var website = [];
-    
-        $('body').each(function(i, elem){
-              name = $(elem).find('.fn').text();
-              address = $(elem).find('.preptime').text();
-              website = $(elem).find('.m_content_recette_ingredients').text();
-              console.log(name+"±"+address+"±"+website);}
-         )
-        }
-        }, interval * i, i);
-        })
-        }
+//Noembed test
+searchTerm="https://soundcloud.com/aboveandbeyond/abgt235";
+url="https://noembed.com/embed?url="+ searchTerm;// +"callback=my_embed_function";
+
+var request = require('request');
+proxy='http://proxy.intra.bt.com:8080';
+
+function getSCurl(search){
+url="https://noembed.com/embed?url="+ search;
+request({'url':url,'proxy':proxy,headers: {'User-Agent': 'MY IPHONE 7s'}},function(error,response,html){
+  //console.log(JSON.parse(html)['html']);
+  
+});
+}
