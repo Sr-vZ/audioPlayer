1f0304546ee70bbb3ff880c1a474a4cbbcebc6a0
Sourav De <srvz39@gmail.com>
2019-07-11 14:59:10 +0530

    sync commit

--- test.js
+++ test.js
@@ -3,6 +3,8 @@ searchTerm="https://soundcloud.com/aboveandbeyond/abgt235";
 url="https://noembed.com/embed?url="+ searchTerm;// +"callback=my_embed_function";
 
 var request = require('request');
+var cheerio = require('cheerio');
+
 proxy='http://proxy.intra.bt.com:8080';
 
 function getSCurl(search){
@@ -12,3 +14,16 @@ request({'url':url,'proxy':proxy,headers: {'User-Agent': 'MY IPHONE 7s'}},functi
   
 });
 }
+function searchMusic(search){
+  url="https://soundcloud.com/search/sounds?q="+search;
+  request({'url':url,'proxy':proxy,headers: {'User-Agent': 'MY IPHONE 7s'}},function(error,response,html){
+  //console.log(html);
+  var $ = cheerio.load(html);
+  $('.lazyLoadingList__list sc-list-nostyle sc-clearfix').filter(function(i,el){
+    var data = $(this);
+    console.log(data);
+  });
+  console.log($('ul').text());
+});
+}
+searchMusic('above and beyond');
